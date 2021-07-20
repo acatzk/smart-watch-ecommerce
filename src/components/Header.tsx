df7492be4650b82052a9d4e24bdd36d29806eae5
acatzk <joshuaimalay@gmail.com>
2021-07-20 11:37:11 +0800

    ✨ FEATURE: Implement dark mode style (#6)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,38 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useTheme } from 'next-themes'
 import { MenuIcon } from '~/utils/Icon'
 import React, { useState } from 'react'
 import Sidebar from '~/components/Sidebar'
+import { LogoDark, LogoLight } from './Logo'
 
 const Header: React.FC<{}> = () => {
+  const { theme } = useTheme()
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <React.Fragment>
-      <header className="sticky top-0 bg-white z-10 flex items-center justify-center w-full pt-4 md:pt-6">
+      <header className="sticky top-0 bg-white dark:bg-[#08162A] z-10 flex items-center justify-center w-full pt-4 md:pt-6 transition ease-out duration-700">
         <div className="w-full md:w-1/3 px-4 md:px-0">
           <button onClick={() => setIsOpen(true)} className="active:scale-95">
-            <MenuIcon className="w-6 h-6 text-black" />
+            <MenuIcon className="w-6 h-6 text-black dark:text-white" />
           </button>
         </div>
         <div className="relative w-[270px] h-[56px] block md:hidden">
-          <Image
-            src="/svgs/logo-dark.svg"
-            layout="fill"
-            objectFit="fill"
-            alt="main-logo"
-            className="flex-shrink-0 inset-0 w-full h-full"
-          />
+          {theme === 'light' ? (
+            <LogoDark alt="mobile-header-logo-dark" />
+          ) : (
+            <LogoLight alt="mobile-header-logo-light" />
+          )}
         </div>
         <div className="w-full md:w-1/2 px-4 md:px-0">
           <div className="flex items-center">
             <div className="relative w-[134px] h-[56px] hidden md:block">
-              <Image
-                src="/svgs/logo-dark.svg"
-                layout="fill"
-                objectFit="fill"
-                alt="main-logo"
-                className="flex-shrink-0 inset-0 w-full h-full"
-              />
+              {theme === 'light' ? (
+                <LogoDark alt="desktop-header-logo-dark" />
+              ) : (
+                <LogoLight alt="desktop-header-logo-light" />
+              )}
             </div>
             <nav className="ml-auto">
               <ul className="flex items-center space-x-4 lg:space-x-8 xl:space-x-14 text-sm">
